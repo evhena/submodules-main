05bbed43f74bcf2199d64e04dfee6d37e7277487
Yevheniia <zhenya.bohdanova@scrumlaunch.com>
2024-05-14 15:19:17 +0200

    added sub-module

--- src/App.js
+++ src/App.js
@@ -3,59 +3,34 @@ import './App.css';
 // import testfunc from '../public/submodule-1/src/testmodule';
 // import * as example from '../example-submodule/app';
 // import { sayHello } from './testOutput';
-import TrimVideoModal from 'gogipper_trim_video_modal';
+// import TrimVideoModal from 'gogipper_trim_video_modal';
 
 function App() {
-  // testfunc();
-  // example();
-  // sayHello();
-  const readVideoFile = (input) => {
-    const fr = new FileReader();
-    fr.readAsDataURL(input);
-    fr.addEventListener('load', () => {
-      return fr.result;
-    });
-  };
-  const videoBlobURL = () => {
-    const videoUrl =
-      'https://d2tyu887lcxnka.cloudfront.net/7EE413E4E7B29F573B758AA568F7B1E9.mp4';
-    fetch(videoUrl)
-      .then((res) => res.blob())
-      .then((blob) => {
-        const newBlob = new Blob([blob], { type: 'video/mp4' });
-        readVideoFile(newBlob);
-        let blobUrl = URL.createObjectURL(newBlob);
-        console.log('blobUrl', blobUrl);
-        return blobUrl;
-      });
-  };
-  console.log('videoBlobURL', videoBlobURL());
-  const closeVideoTrim = () => {};
-  const applyTrimmingHandler = () => {};
-  const isVideoHasAudio = false;
+  async function load() {
+    let hi = await import('./sub-module/dist/hi.bundle.js');
+    hi(); // Hello!
+  }
+
+  const element = document.createElement('div');
+  const btn = document.createElement('button');
+
+  element.innerHTML = 'I have changed!';
+
   return (
     <div className="App">
-      <TrimVideoModal
-        isOpenTrimModal={true}
-        closeVideoTrim={closeVideoTrim}
-        videoBlobURL={videoBlobURL()}
-        applyTrimmingHandler={applyTrimmingHandler}
-        isVideoHasAudio={isVideoHasAudio}
-      />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={logo} className="App-logo" alt="logo" />
+      <button onClick={() => load()}>Click me</button>
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
     </div>
   );
 }
